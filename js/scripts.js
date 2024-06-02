daf5db8fa7e525cb2cf2729d2fb573ef46e73d91
GSiame <156589976+GSiame@users.noreply.github.com>
2024-06-02 23:51:42 +0100

    Updated script.js
    Changed/deleted all existing code.
    Changed/created a new variable/pokemonList.
    Added five objects to the array/each object represents one pokemon.
    Added four keys to each object/name, type, profile, evolutions.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,38 @@
-alert('Hello world');
+// array for pokemon data to display in the app
+let pokemonList = [ 
 
-let favoriteFood = 'Steak';
-document.write(favoriteFood);
+     {name: 'Charmander', 
+      type: ['FIRE', 'Lizard Pokémon', 'The fire on the tip of its tail is a measure of its life. If healthy, its tail burns intensely.'], 
+      profile: [ {height: 0.6, weight: 8.5, abilities:['Blaze', 'Solar-power'] }], 
+      evolutions: ['Charmander evolves into Charmeleon at level 16.']},
+
+     {name: 'Druddigon', 
+      type: ['DRAGON', 'Cave Pokémon', 'It runs through the narrow tunnels formed by Excadrill and Onix. It uses its sharp claws to catch prey.'], 
+      profile: [ {height: 1.6, weight: 139, abilities:['Rough-skin', 'Mold-breaker', 'Sheer-force'] }], 
+      evolutions: ['Druddigon has no evolutions.']},
+    
+     
+     {name: 'Ivysaur', 
+      type: ['GRASS', 'POISON', 'Seed Pokémon', 'When the bud on its back starts swelling, a sweet aroma wafts to indicate the flowers coming bloom.'], 
+      profile: [ {height: 1, weight: 13, abilities:['Chlorophyll', 'Overgrow'] }],  
+      evolutions: ['Bulbasaur evolves into Ivysaur at level 16.', 'Ivysaur evolves into Venusaur at level 32.']},
+    
+     {name: 'Quagsire', 
+      type: ['WATER', 'GROUND', 'Water Fish Pokémon', 'It has an easygoing nature. It doesnt care if it bumps its head on boats and boulders while swimming.'], 
+      profile: [ {height: 1.4, weight: 75, abilities:['Damp', 'Water-absorb', 'Unaware'] }], 
+      evolutions: ['Wooper evolves into Quagsire at level 20.']},
+
+     {name: 'Zangoose', 
+      type: ['NORMAL', 'Cat Weasel Pokémon', 'Its Sevipers archrival. To threaten those it encounters, it fans out the claws on its front paws.'], 
+      profile: [ {height: 1.3, weight: 40.3, abilities:['Immunity', 'Toxic-boost'] }], 
+      evolutions: ['Zangoose has no evolutions.']},
+]; 
+
+
+/*
+    {name: '', 
+     type: ['', '', ''], 
+     profile: [ {height: , weight: , abilities:['', ''] }], 
+     evolutions: ['']},
+*/
+   
